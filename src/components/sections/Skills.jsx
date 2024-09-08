b50d2f8df2bc70ec4ba7bf2f3c06fbdc14fbddee
kevin-was-here <code --wait>
2024-09-08 06:13:20 -0600

    skills section and an attempt at fix ing padding but it sucks ass

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,19 +1,16 @@
 import data from "../../data/skills.json"
+import '../style/Skill.css'
 
 export default function Skills(){
     return(
         <section id="skillsSection" className="skills-section">
             <div className="skills-section-content-box">
-                <h1 className="skills-seciton-heading">My technologies</h1>
+                <h1 className="skills-section-heading">My technologies</h1>
                 <div className="skills-section-container">
                     {data?.skills?.map((item,index) => (
                         <div key={index} className="skill-section-block"> 
-                            <div className="skills-section-img">
-                                <img src={item.src} alt="skill icon"/>    
-                            </div>
-                            <div className="skills-section-item-text">
-                                <h6>{item.title}</h6>
-                            </div>
+                                <img src={item.src} alt="skill icon"/>
+                                <h6 className="skills-section-item-text">{item.title}</h6>    
                         </div>
                     ))}
                 </div>
