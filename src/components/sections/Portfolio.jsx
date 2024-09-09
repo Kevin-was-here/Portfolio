44b2502fec1e7ed7eac4f6615dae87e634e76971
Kevin_was_here <xuyang4@ualberta.ca>
2024-09-09 05:55:19 -0600

    so far so good

---
+++ src/components/sections/Portfolio.jsx
@@ -0,0 +1,12 @@
+import data from "../../data/projects.json"
+import "../style/Portfolio.css" 
+
+export default function Portfolio(){
+    return(
+        <section id="portfolioSection" className="portfolio-section">
+            <div className="portfolio-content-box">
+                <h1 className="portfolio-section-heading">My Portfolio</h1>
+            </div>
+        </section>
+    )
+}
