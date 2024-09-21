97d8fb3fcb2fa68927035818fa8d2aec46a2db12
kevin-was-here <code --wait>
2024-09-21 15:48:03 -0600

    fixed, doing styling

---
+++ src/components/sections/WorkExp.jsx
@@ -0,0 +1,34 @@
+import data from "../../data/workexp.json"
+import '../style/WorkExp.css'
+
+export default function WorkExp(){
+    return(
+        <section id="workExpSection" className="workexp-section">
+            <div className="workexp-section-content-box">
+                <h1 className="workexp-section-heading">Work Experience</h1>
+                <div className="workexp-section-container">
+                    {data?.workexp?.map((item,index) => (
+                        <div key={index} className="workexp-section-card"> 
+                            <div classname = "workexp-company-position-duration-box">
+                                <div className="workexp-company-position-box"> 
+                                    <h3 className="workexp-company-title"> {item.company} </h3>
+                                    <h4 className="workexp-position-title"> {item.position} </h4>
+                                </div>
+                                <h4 className="workexp-duration"> {item.start} - {item.end} </h4>
+                            </div>
+                            <div classname = "workexp-bullet-points-box">
+                                {item.bulletPoints?.map((point, pointIndex) => (
+                                    <div key={pointIndex} className="workexp-bullet-point">
+                                        <span className="workexp-bullet-point-icon"> &#8226; </span>
+                                        <span className="workexp-bullet-point-text"> {point} </span>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+
+}
