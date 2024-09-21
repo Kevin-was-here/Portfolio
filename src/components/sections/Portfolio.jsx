a74c43045fdffb1836fd27b6d57b814e5efdf6a3
kevin-was-here <code --wait>
2024-09-20 19:23:59 -0600

    projects section done!!

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -7,6 +7,26 @@ export default function Portfolio(){
             <div className="portfolio-content-box">
                 <h1 className="portfolio-section-heading">My Portfolio</h1>
             </div>
+            <div className="portfolio-section-container">
+                {data?.projects?.map((item,index) => (
+                    <div key={index}className="portfolio-section-card">
+                        <h3 className="portfolio-section-project-title">{item.title}</h3>
+                        <div className="portfolio-section-card-img">
+                            <img src={item.src} alt="placeholder" />
+                        </div>
+                        <div className="portfolio-section-card-tools"> 
+                            {item.tools?.map((tool, toolIndex) => (
+                                    <span key={toolIndex} className="portfolio-section-card-tool-block">
+                                        {tool}
+                                    </span>
+                                ))}
+                        </div>
+                        <div className="portfolio-section-card-description">{item.description}</div>
+                        <a className="portfolio-section-card-github-img" href={item.link}><img src='./github.png'/>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
