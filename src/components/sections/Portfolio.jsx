32c4ef04b843deb17d29820127479eaefac4ccf8
kevin-was-here <code --wait>
2024-09-20 19:27:16 -0600

    how dare you mess up my portfolio

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -5,22 +5,6 @@ export default function Portfolio(){
     return(
         <section id="portfolioSection" className="portfolio-section">
             <h1 className="portfolio-section-heading">My Portfolio</h1>
-            
-            <div className="portfolio-section-container">
-                {data?.projects?.map((item,index) => (
-                    <div key={index} className="portfolio-section-card">
-                        <div className="portfolio-section-img">
-                            <img src={item.src} alt="placeholder" />
-                        </div>
-                        <div className="portfolio-section-card-content">
-                            <div>
-                                <h3 className="portfolio-section-title">{item.title}</h3>
-                                <p className="text-md">{item.description}</p>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
             <div className="portfolio-section-container">
                 {data?.projects?.map((item,index) => (
                     <div key={index}className="portfolio-section-card">
