6a16b225ac5887c52e98479aa1ff77ef7213614c
kevin-was-here <code --wait>
2024-09-21 11:46:50 -0600

    time for navbar styles

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -44,13 +44,13 @@ function Navbar(){
             <div className = {`navbar-items ${navActive ? "active" : ""}`}>
                 <ul>
                     <li>
-                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="heroSection" className="navbar-content">Home</Link>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-100} duration={500} to="heroSection" className="navbar-content">Home</Link>
                     </li>
                     <li>
-                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="skillsSection" className="navbar-content">Tech</Link>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-100} duration={500} to="skillsSection" className="navbar-content">Tech</Link>
                     </li>
                     <li>
-                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="portfolioSection" className="navbar-content">Portfolio</Link>
+                        <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-100} duration={500} to="portfolioSection" className="navbar-content">Portfolio</Link>
                     </li>
                     <li>
                         <Link onClick={closeMenu} activeClass="navbar-active-content" spy={true} smooth={true} offset={-70} duration={500} to="Void" className="navbar-content">Void</Link>
